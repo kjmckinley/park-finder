998c381eb378d5f54ede5cfc7cfb1907c66985d9
Noah <mcbeasty11.nh@icloud.com>
2021-03-30 20:42:40 -0500

    mapbox directions top of body in index

--- assests/js/script.js
+++ assests/js/script.js
@@ -14,8 +14,6 @@ let parkSearch = function (park) {
     })
 };
 
-// let lat = 30.4390489;
-// let lng = -97.7360259;
 mapboxgl.accessToken = 'pk.eyJ1Ijoibml6enlubyIsImEiOiJja21zanJpeWYwaWwzMm9wZnlpbWtzMG1vIn0.OgJmZwe7N5Zy3QWgnhM4vw';
 
 // navigator.geolocation.getCurrentPosition(successLocation); get current location
@@ -55,57 +53,74 @@ function setUpMap(center) {
 
 };
 
+// Add event listener to search button
+// let buttonEl = document.getElementById("find-park");
+// buttonEl.addEventListener("click", function () {
+//     console.log(parkSearch);
+// });
+
+// get.addEventListener("click", function () {
+//     // call api
+// });
+
+// let lat = 30.4390489;
+// let lng = -97.7360259;
+
+// let marker = new mapboxgl.Marker()
+// .setLngLat([lng, lat])
+// .addTo(map);
+
 // Park search click event
 
-var searchEl = 
-parkSearch 
+// var searchEl = 
+// parkSearch 
 
-var geojson = {
-    type: 'FeatureCollection',
-    features: [
-        {
-            type: 'Feature',
-            geometry: {
-                type: 'Point',
-                coordinates: [38.9071923, -77.0368707 ]
-            },
-            properties: {
-                title: 'Mapbox',
-                description: 'Washington, D.C.'
-            }
-        },
-        {
-            type: 'Feature',
-            geometry: {
-                type: 'Point',
-                coordinates: [37.7749295, -122.4194155]
-            },
-            properties: {
-                title: 'Mapbox',
-                description: 'San Francisco, Ca'
-            }
-        }
-    ]
-};
+// var geojson = {
+//     type: 'FeatureCollection',
+//     features: [
+//         {
+//             type: 'Feature',
+//             geometry: {
+//                 type: 'Point',
+//                 coordinates: [38.9071923, -77.0368707 ]
+//             },
+//             properties: {
+//                 title: 'Mapbox',
+//                 description: 'Washington, D.C.'
+//             }
+//         },
+//         {
+//             type: 'Feature',
+//             geometry: {
+//                 type: 'Point',
+//                 coordinates: [37.7749295, -122.4194155]
+//             },
+//             properties: {
+//                 title: 'Mapbox',
+//                 description: 'San Francisco, Ca'
+//             }
+//         }
+//     ]
+// };
 
-// add markers to map
-geojson.features.forEach(function (marker) {
-    // create a DOM element for the marker
-    var el = document.createElement('div');
-    console.log(marker.geometry.coordinates);
-    el.className = 'marker';
-    el.style.backgroundImage =
-'url(https://placekitten.com/g/' +
-marker.properties.iconSize.join('/') +
-'/)';
-el.style.width = marker.properties.iconSize[0] + 'px';
-el.style.height = marker.properties.iconSize[1] + 'px';
-el.style.backgroundSize = '100%';
-    // add marker to map
-    new mapboxgl.Marker(el)
-        .setLngLat(marker.geometry.coordinates)
-        .addTo(map);
-});
+// // add markers to map
+// geojson.features.forEach(function (marker) {
+//     // create a DOM element for the marker
+//     var el = document.createElement('div');
+//     console.log(marker.geometry.coordinates);
+//     el.className = 'marker';
+//     el.style.backgroundImage =
+// 'url(https://placekitten.com/g/' +
+// marker.properties.iconSize.join('/') +
+// '/)';
+// el.style.width = marker.properties.iconSize[0] + 'px';
+// el.style.height = marker.properties.iconSize[1] + 'px';
+// el.style.backgroundSize = '100%';
+//     // add marker to map
+//     new mapboxgl.Marker(el)
+//         .setLngLat(marker.geometry.coordinates)
+//         .addTo(map);
+// });
 
 setUpMap();
 parkSearch();
