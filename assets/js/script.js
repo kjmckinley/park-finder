aa8adf155b34c72b7ad75d47d637d71a086bed7f
Kyle <kj.mckinley@gmail.com>
2021-04-04 21:31:30 -0500

    first branch commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,8 +74,8 @@ function displayResults(responseJson, maxResults) {
     $('.list-of-results').empty();
     // Looping through the response and formatting results
     for (let i = 0; i < responseJson.data.length & i < maxResults; i++) {
-        $('.list-of-results').append(`<li><h3><a class ='park-title' target = '_blank' href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3><input
-        <p class='park-description'>${responseJson.data[i].description}</p>
+        $('.list-of-results').append(`<li><h3><a class ='park-title' target = '_blank' href="${responseJson.data[i].url}">${responseJson.data[i].fullName}</a></h3>
+        <p class='park-description'>${responseJson.data[i].description}</p><input type="submit" value="Directions!">
         </li>`);
     }
 
